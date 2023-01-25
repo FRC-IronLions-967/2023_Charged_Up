1a47e09ec91f6b7577ebf083170cdd0e8d283467
caryon26 <122171607+Beck-Daly@users.noreply.github.com>
2023-01-24 18:45:36 -0600

    pnuematic from frc

--- 2023_Charged_Up/src/main/java/frc/robot/subsystems/PnuematicSubsystem.java
+++ 2023_Charged_Up/src/main/java/frc/robot/subsystems/PnuematicSubsystem.java
@@ -1,10 +1,28 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PnuematicSubsystem extends SubsystemBase {
+
+    private Compressor pcmCompressor;
+    private Compressor phCompressor;
+    private boolean enabled;
+    private boolean pressureSwitch;
+    private double current;
     
     public PnuematicSubsystem() {
+        pcmCompressor  = new Compressor(0, PneumaticsModuleType.CTREPCM);
+        phCompressor = new Compressor(1, PneumaticsModuleType.REVPH);
+
+        pcmCompressor.enableDigital();
+        pcmCompressor.disable();
+
+        enabled = pcmCompressor.isEnabled();
+        pressureSwitch = pcmCompressor.getPressureSwitchValue();
+        current = pcmCompressor.getCurrent();
+
     }
 
     @Override
