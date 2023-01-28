7410044c013f234895685c193445b03fe4e78485
caryon26 <122171607+Beck-Daly@users.noreply.github.com>
2023-01-27 19:01:19 -0600

    Update PnuematicSubsystem.java

--- 2023_Charged_Up/src/main/java/frc/robot/subsystems/PnuematicSubsystem.java
+++ 2023_Charged_Up/src/main/java/frc/robot/subsystems/PnuematicSubsystem.java
@@ -1,7 +1,11 @@
 package frc.robot.subsystems;
 
+import javax.xml.crypto.dsig.keyinfo.KeyInfoFactory;
+
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PnuematicSubsystem extends SubsystemBase {
@@ -11,7 +15,16 @@ public class PnuematicSubsystem extends SubsystemBase {
     private boolean enabled;
     private boolean pressureSwitch;
     private double current;
+    private DoubleSolenoid DoublePCM;
+    private DoubleSolenoid DoublePH;
     
+
+    public Value kOff;
+    public Value kForward;
+    public Value kReverse;
+
+
+
     public PnuematicSubsystem() {
         pcmCompressor  = new Compressor(0, PneumaticsModuleType.CTREPCM);
         phCompressor = new Compressor(1, PneumaticsModuleType.REVPH);
@@ -23,6 +36,13 @@ public class PnuematicSubsystem extends SubsystemBase {
         pressureSwitch = pcmCompressor.getPressureSwitchValue();
         current = pcmCompressor.getCurrent();
 
+        DoublePCM = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
+        DoublePH = new DoubleSolenoid(9, PneumaticsModuleType.REVPH, 4, 5);
+
+
+        DoublePCM.set(kOff);
+        DoublePCM.set(kForward);
+        DoublePCM.set(kReverse);
     }
 
     @Override
