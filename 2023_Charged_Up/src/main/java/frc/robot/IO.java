97855afcc786091979156f4e1c247e9d355aefa7
lbeanz15 <122175200+lbeanz15@users.noreply.github.com>
2023-02-04 12:33:10 -0600

    Update IO.java

--- 2023_Charged_Up/src/main/java/frc/robot/IO.java
+++ 2023_Charged_Up/src/main/java/frc/robot/IO.java
@@ -19,12 +19,10 @@ public static IO getInstance() {
     return instance;
 }
 public void teleopInt(){
-    manipulatorController.whenButtonPressed("SELECT", new RunPnuematicsCommand(0));
-    manipulatorController.whenButtonPressed("START", new RunPnuematicsCommand(1));
-    // manipulatorController.whenButtonPressed("X", new TogglePnuematicsCommand(0));
-    // manipulatorController.whenButtonReleased("X", new TogglePnuematicsCommand(2));
-    // manipulatorController.whenButtonPressed("A", new TogglePnuematicsCommand(1));
-    // manipulatorController.whenButtonReleased("A", new TogglePnuematicsCommand(2));
+    manipulatorController.whenButtonPressed("X", new TogglePnuematicsCommand(0));
+    manipulatorController.whenButtonReleased("X", new TogglePnuematicsCommand(2));
+    manipulatorController.whenButtonPressed("A", new TogglePnuematicsCommand(1));
+    manipulatorController.whenButtonReleased("A", new TogglePnuematicsCommand(2));
 }
 public XBoxController getDriverController(){
     return driverController;
