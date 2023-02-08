7785f4307af9149419dfec032c3c82cc1f71ef18
caryon26 <122171607+Beck-Daly@users.noreply.github.com>
2023-02-07 18:38:16 -0600

    Update PnuematicSubsystem.java

--- 2023_Charged_Up/src/main/java/frc/robot/subsystems/PnuematicSubsystem.java
+++ 2023_Charged_Up/src/main/java/frc/robot/subsystems/PnuematicSubsystem.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PnuematicSubsystem extends SubsystemBase {
@@ -18,22 +19,28 @@ public class PnuematicSubsystem extends SubsystemBase {
         pcmCompressor  = new Compressor(1, PneumaticsModuleType.CTREPCM);
 
         doubleSol = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+    
+        doubleSol.set(kReverse);
     }
 
     public void toggleSolenoid(Integer toggleSolenoid){
-        if(toggleSolenoid == 0){
-            doubleSol.set(kForward);
-        } else if(toggleSolenoid == 1){
-            doubleSol.set(kReverse);
-        }else if(toggleSolenoid == 2){
-            doubleSol.set(kOff);
-        }
+        // if(toggleSolenoid == 0){
+        //     doubleSol.set(kForward);
+        // } else if(toggleSolenoid == 1){
+        //     doubleSol.set(kReverse);
+        // }else if(toggleSolenoid == 2){
+        //     doubleSol.set(kOff);
+        // }
+        doubleSol.set(kForward);
         doubleSol.toggle();
     }
    
 
     @Override
     public void periodic() {
-   
+        doubleSol.set(kForward);
+        doubleSol.toggle();
+        System.out.println(doubleSol.get());
+        System.out.println(doubleSol.isRevSolenoidDisabled());
     }
 }
