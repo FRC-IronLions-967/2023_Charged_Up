deaab802b3ef63bed24bc3969ba0a01823ff21af
caryon26 <122171607+Beck-Daly@users.noreply.github.com>
2023-02-18 09:39:29 -0600

    quick drive changes

--- 2023_Charged_Up/src/main/java/frc/robot/subsystems/PnuematicSubsystem.java
+++ 2023_Charged_Up/src/main/java/frc/robot/subsystems/PnuematicSubsystem.java
@@ -1,59 +1,47 @@
-package frc.robot.subsystems;
+// package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
-import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
+// import edu.wpi.first.wpilibj.Compressor;
+// import edu.wpi.first.wpilibj.DoubleSolenoid;
+// import edu.wpi.first.wpilibj.PneumaticsModuleType;
+// import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class PnuematicSubsystem extends SubsystemBase {
+// public class PnuematicSubsystem extends SubsystemBase {
 
-    private Compressor pcmCompressor;
-    private DoubleSolenoid clawSol;
-    private DoubleSolenoid armSol_1;
-    //private DoubleSolenoid armSol_2;
+//     private Compressor pcmCompressor;
+//     private DoubleSolenoid doubleSol;
 
     
 
-    public PnuematicSubsystem() {
-        try{
-            pcmCompressor  = new Compressor(1, PneumaticsModuleType.CTREPCM);
+//     public PnuematicSubsystem() {
+//         pcmCompressor  = new Compressor(1, PneumaticsModuleType.CTREPCM);
 
-            clawSol = new DoubleSolenoid(1, PneumaticsModuleType.CTREPCM, 2, 3);
-            armSol_1 = new DoubleSolenoid(1, PneumaticsModuleType.CTREPCM, 0, 1);
-            //armSol_2 = new DoubleSolenoid(1, PneumaticsModuleType.CTREPCM, 1, 6);
-        
-        } catch(Exception e) {
-            System.out.println("Unable to created DoubleSolenoid");
-        }
-    }
+//         doubleSol = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+    
+//         doubleSol.set(kForward);
+//         doubleSol.set(kReverse);
+//     }
 
-    public void toggleClaw(boolean direction){
-        if(direction){
-            clawSol.set(kForward);
-        } else {
-            clawSol.set(kReverse);
-        }
-    }
-
-    public void toggleArm(boolean direction){
-        if(direction){
-           armSol_1.set(kForward);
-           //armSol_2.set(kForward);
-        } else {
-            armSol_1.set(kReverse);
-            //armSol_2.set(kReverse);
-        }
-    }
+//     public void toggleSolenoid(Integer toggleSolenoid){
+//         // if(toggleSolenoid == 0){
+//         //     doubleSol.set(kForward);
+//         // } else if(toggleSolenoid == 1){
+//         //     doubleSol.set(kReverse);
+//         // }else if(toggleSolenoid == 2){
+//         //     doubleSol.set(kOff);
+//         // }
+//         doubleSol.set(kForward);
+//         doubleSol.toggle();
+//     }
    
-    //hello!
 
-    @Override
-    public void periodic() {
-        //System.out.println(doubleSol.get());
-        //System.out.println(doubleSol.isRevSolenoidDisabled());
-    }
-}
+//     @Override
+//     public void periodic() {
+//         doubleSol.set(kForward);
+//         doubleSol.toggle();
+//         System.out.println(doubleSol.get());
+//         System.out.println(doubleSol.isRevSolenoidDisabled());
+//     }
+// }
