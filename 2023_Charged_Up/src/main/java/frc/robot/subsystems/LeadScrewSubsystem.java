dfabf22e70abb7e973d73473071855ae1c672244
Gabriel Marsh <90068519+gmarsh-coding@users.noreply.github.com>
2023-03-09 15:56:42 -0600

    different gear ratio

--- 2023_Charged_Up/src/main/java/frc/robot/subsystems/LeadScrewSubsystem.java
+++ 2023_Charged_Up/src/main/java/frc/robot/subsystems/LeadScrewSubsystem.java
@@ -33,7 +33,7 @@ public class LeadScrewSubsystem extends SubsystemBase {
     private double mmPerInch = 25.4;
     private double leadScrewPitch = 8; //2mm pitch, 4 start, 8mm per revolution
     private double leadScrewRevPerInch = mmPerInch / leadScrewPitch; //3.175 revolution for 1 inch approx.
-    private double leadScrewGearboxRatio = 9.0; //modify as hardware changes, current ratio is 9:1
+    private double leadScrewGearboxRatio = 5.0; //modify as hardware changes, current ratio is 9:1
 
     public LeadScrewSubsystem() {
         leadScrew = new CANSparkMax(5, MotorType.kBrushless);
